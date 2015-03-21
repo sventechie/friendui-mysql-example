c3d4d4a99206606b04e400979aa129f052587ebb
Sven Pedersen <sven.pedersen@crossroadsmusic.com>
2015-03-20 20:50:46 -0400

    templates + code for them

--- project.clj
+++ project.clj
@@ -5,7 +5,9 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.3.2"]
                  [ring/ring-defaults "0.1.4"]
-                 [sventechie/friendui-mysql "0.0.2-SNAPSHOT"]]
+                 [enlive "1.1.5"] ; html templating
+                 [vlacs/dossier "0.1.4"] ; enlive utils
+                 [sventechie/friendui-mysql "0.0.3-SNAPSHOT"]]
   :plugins [[lein-ring "0.8.13"]]
   :ring {:handler friendui-mysql-example.handler/app}
   :profiles
--- src/friendui_mysql_example/handler.clj
+++ src/friendui_mysql_example/handler.clj
@@ -3,17 +3,19 @@
             [compojure.route :as route]
             [compojure.handler :as handler]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [net.cgrand.enlive-html :as html :refer [deftemplate content substitute clone-for]]
+            [dossier.utils :as utils]
             [cemerick.friend :as friend]
             [cemerick.friend [workflows :as workflows]
                              [credentials :as creds]]
             [de.sveri.friendui.routes.user :refer [friend-routes]]
-            [sventechie.friendui-mysql.db :refer :all]
+            [de.sveri.friendui.globals :as f-global]
+            [sventechie.friendui-mysql.db :refer [get-usermap-by-username]]
             [sventechie.friendui-mysql.storage :refer :all]
-            [sventechie.friendui-mysql.globals :refer :all])
-
+            [sventechie.friendui-mysql.globals :refer [friendui-config]])
   (:import [sventechie.friendui-mysql.storage.FrienduiStorage]))
 
-(def FrienduiStorageImpl (->FrienduiStorage database))
+(def FrienduiStorageImpl (->FrienduiStorage (:database friendui-config)))
 
 (def friend-settings
   {:credential-fn             (partial creds/bcrypt-credential-fn get-usermap-by-username)
@@ -22,11 +24,23 @@
    :unauthorized-redirect-uri "/user/login"
    :default-landing-uri       "/"})
 
+(def template-path "templates/de/sveri/friendui/user/")
+;(alter-var-root #'f-global/template-path template-path)
+(html/deftemplate base (str template-path "base.html")
+                  [{:keys [title main]}]
+                  [:#title] (utils/maybe-content title)      ; :base-template-title-key key in the config
+                  [:#content] (utils/maybe-substitute main)) ; :base-template-content-key key in the config
+
+(alter-var-root #'f-global/base-template (fn [_] (partial base)))
+
 (defn authenticate-routes
   "Add Friend handler to routes"
   [routes-set]
   (handler/site
-        (friend/authenticate routes-set friend-settings)))
+    (friend/authenticate routes-set friend-settings)))
+
+(def friendui-callbacks {:signup-succ-func (fn[] "success")
+                         :activate-account-succ-func (fn[user-map] "activated")})
 
 (defroutes app-routes
   (GET "/" [] "Hello World")
